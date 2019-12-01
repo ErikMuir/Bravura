01e88888f02252b3fd2c44699bef87d950f20e81
Erik Muir <erikdmuir@gmail.com>
2019-12-01 17:41:42 -0600

    adjust bravura exception default message; remove resharper settings file

--- Bravura/Exceptions/BravuraException.cs
+++ Bravura/Exceptions/BravuraException.cs
@@ -4,7 +4,7 @@ namespace Bravura.Exceptions
 {
     public class BravuraException : Exception
     {
-        public BravuraException() : base("Unknown Bravura exception") { }
+        public BravuraException() : base("Unknown error") { }
         public BravuraException(string message) : base(message) { }
         public BravuraException(string message, Exception innerException) : base(message, innerException) { }
     }
