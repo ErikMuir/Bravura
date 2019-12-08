7989daf175864915cdc4f23c5a2f4f532b6e303d
Erik Muir <erikdmuir@gmail.com>
2019-12-07 23:32:37 -0600

    remove unused equality overrides for Interval

--- Bravura.Tonality/Interval.cs
+++ Bravura.Tonality/Interval.cs
@@ -25,27 +25,5 @@
         public override string ToString() => $"{Accidental.Symbol}{NoteIndex}";
 
         public string ToAsciiString() => $"{Accidental.AsciiSymbol}{NoteIndex}";
-
-        //public static bool operator ==(Interval a, Interval b)
-        //{
-        //    if (a == null && b == null) return true;
-        //    if (a == null || b == null) return false;
-        //    return a.NoteIndex == b.NoteIndex && a.Accidental.SemitonesAwayFromNatural == b.Accidental.SemitonesAwayFromNatural;
-        //}
-
-        //public static bool operator !=(Interval a, Interval b)
-        //{
-        //    return !(a == b);
-        //}
-
-        //public override bool Equals(object obj)
-        //{
-        //    if (!(obj is Interval)) return false;
-        //    var interval = (Interval)obj;
-        //    return this.NoteIndex == interval.NoteIndex && this.Accidental.SemitonesAwayFromNatural == interval.Accidental.SemitonesAwayFromNatural;
-        //}
-
-        //public override int GetHashCode()
-        //    => this.NoteIndex + this.Accidental.SemitonesAwayFromNatural;
     }
 }
