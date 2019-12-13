defeb3197491adb166c1ebb33f337eeb95c5ab7b
Erik Muir <erikdmuir@gmail.com>
2019-12-12 22:03:16 -0600

    refactor Pitch unit tests

--- Bravura.Tests/Tonality/PitchTests.cs
+++ Bravura.Tests/Tonality/PitchTests.cs
@@ -1,57 +1,72 @@
-using System.Collections.Generic;
 using Xunit;
 
 namespace Bravura.Tonality.Tests
 {
     public class PitchTests
     {
-        public static IEnumerable<object[]> PitchMemberData()
+        [Fact]
+        public void SemitonesAboveC_Test()
         {
-            yield return new object[] { Pitches.CDoubleFlat, Pitches.BFlat };
-            yield return new object[] { Pitches.DDoubleFlat, Pitches.CNatural };
-            yield return new object[] { Pitches.EDoubleFlat, Pitches.DNatural };
-            yield return new object[] { Pitches.FDoubleFlat, Pitches.EFlat };
-            yield return new object[] { Pitches.GDoubleFlat, Pitches.FNatural };
-            yield return new object[] { Pitches.ADoubleFlat, Pitches.GNatural };
-            yield return new object[] { Pitches.BDoubleFlat, Pitches.ANatural };
-            yield return new object[] { Pitches.CFlat, Pitches.BNatural };
-            yield return new object[] { Pitches.DFlat, Pitches.DFlat };
-            yield return new object[] { Pitches.EFlat, Pitches.EFlat };
-            yield return new object[] { Pitches.FFlat, Pitches.ENatural };
-            yield return new object[] { Pitches.GFlat, Pitches.GFlat };
-            yield return new object[] { Pitches.AFlat, Pitches.AFlat };
-            yield return new object[] { Pitches.BFlat, Pitches.BFlat };
-            yield return new object[] { Pitches.CNatural, Pitches.CNatural };
-            yield return new object[] { Pitches.DNatural, Pitches.DNatural };
-            yield return new object[] { Pitches.ENatural, Pitches.ENatural };
-            yield return new object[] { Pitches.FNatural, Pitches.FNatural };
-            yield return new object[] { Pitches.GNatural, Pitches.GNatural };
-            yield return new object[] { Pitches.ANatural, Pitches.ANatural };
-            yield return new object[] { Pitches.BNatural, Pitches.BNatural };
-            yield return new object[] { Pitches.CSharp, Pitches.CSharp };
-            yield return new object[] { Pitches.DSharp, Pitches.DSharp };
-            yield return new object[] { Pitches.ESharp, Pitches.FNatural };
-            yield return new object[] { Pitches.FSharp, Pitches.FSharp };
-            yield return new object[] { Pitches.GSharp, Pitches.GSharp };
-            yield return new object[] { Pitches.ASharp, Pitches.ASharp };
-            yield return new object[] { Pitches.BSharp, Pitches.CNatural };
-            yield return new object[] { Pitches.CDoubleSharp, Pitches.DNatural };
-            yield return new object[] { Pitches.DDoubleSharp, Pitches.ENatural };
-            yield return new object[] { Pitches.EDoubleSharp, Pitches.FSharp };
-            yield return new object[] { Pitches.FDoubleSharp, Pitches.GNatural };
-            yield return new object[] { Pitches.GDoubleSharp, Pitches.ANatural };
-            yield return new object[] { Pitches.ADoubleSharp, Pitches.BNatural };
-            yield return new object[] { Pitches.BDoubleSharp, Pitches.CSharp };
+            Assert.Equal(0, Pitches.CNatural.SemitonesAboveC);
+            Assert.Equal(1, Pitches.CSharp.SemitonesAboveC);
+            Assert.Equal(1, Pitches.DFlat.SemitonesAboveC);
+            Assert.Equal(2, Pitches.DNatural.SemitonesAboveC);
         }
 
-        [Theory]
-        [MemberData(nameof(PitchMemberData))]
-        public void Pitch_EnharmonicallyEquals_Tests(Pitch pitch, Pitch expectedLogical)
+        [Fact]
+        public void Logical_Test()
         {
-            var actualLogical = pitch.Logical();
-            Assert.True(pitch.EnharmonicallyEquals(actualLogical));
-            Assert.True(pitch.EnharmonicallyEquals(expectedLogical));
-            Assert.True(expectedLogical.EnharmonicallyEquals(actualLogical));
+            Assert.Equal(Pitches.CNatural, Pitches.CNatural.Logical());
+            Assert.Equal(Pitches.CNatural, Pitches.BSharp.Logical());
+            Assert.Equal(Pitches.CNatural, Pitches.DDoubleFlat.Logical());
+            Assert.Equal(Pitches.CSharp, Pitches.CSharp.Logical());
+            Assert.Equal(Pitches.DFlat, Pitches.DFlat.Logical());
+        }
+
+        [Fact]
+        public void GetPitchByIntervalAbove_Test()
+        {
+            Assert.Equal(Pitches.CNatural, Pitches.CNatural.GetPitchByIntervalAbove(Intervals.PerfectUnison));
+            Assert.Equal(Pitches.GNatural, Pitches.CNatural.GetPitchByIntervalAbove(Intervals.PerfectFifth));
+        }
+
+        [Fact]
+        public void GetPitchByIntervalBelow_Test()
+        {
+            Assert.Equal(Pitches.CNatural, Pitches.CNatural.GetPitchByIntervalBelow(Intervals.PerfectUnison));
+            Assert.Equal(Pitches.FNatural, Pitches.CNatural.GetPitchByIntervalBelow(Intervals.PerfectFifth));
+        }
+
+        [Fact]
+        public void EnharmonicallyEquals_Test()
+        {
+            Assert.True(Pitches.CNatural.EnharmonicallyEquals(Pitches.BSharp));
+            Assert.False(Pitches.CNatural.EnharmonicallyEquals(Pitches.BNatural));
+            Assert.False(Pitches.CNatural.EnharmonicallyEquals(null as Pitch));
+        }
+
+        [Fact]
+        public void PitchEquals_Test()
+        {
+            Assert.True(Pitches.CNatural.Equals(Pitches.CNatural));
+            Assert.False(Pitches.CNatural.Equals(Pitches.BSharp));
+            Assert.False(Pitches.CNatural.Equals(null as Pitch));
+        }
+
+        [Fact]
+        public void ObjectEquals_Test()
+        {
+            Assert.True(Pitches.CNatural.Equals((object)Pitches.CNatural));
+            Assert.False(Pitches.CNatural.Equals((object)Pitches.BSharp));
+            Assert.False(Pitches.CNatural.Equals((object)null));
+            Assert.False(Pitches.CNatural.Equals(new { Foo = "bar" }));
+        }
+
+        [Fact]
+        public void GetHashCode_Test()
+        {
+            Assert.Equal(Pitches.CNatural.GetHashCode(), Pitches.CNatural.GetHashCode());
+            Assert.NotEqual(Pitches.CNatural.GetHashCode(), Pitches.BSharp.GetHashCode());
         }
     }
 }
