45933a175412fa452a490fdb1c7bc07da6b39ce8
Erik Muir <erikdmuir@gmail.com>
2019-12-14 23:08:09 -0600

    update BravuraTonalityException

--- Bravura.Tonality/Exceptions/BravuraTonalityException.cs
+++ Bravura.Tonality/Exceptions/BravuraTonalityException.cs
@@ -1,12 +1,14 @@
 using System;
+using System.Collections.Generic;
 using Bravura.Common;
 
 namespace Bravura.Tonality
 {
     public class BravuraTonalityException : BravuraException
     {
-        public BravuraTonalityException() : base("Unknown error") { }
+        public BravuraTonalityException() : base() { }
         public BravuraTonalityException(string message) : base(message) { }
+        public BravuraTonalityException(List<string> messages) : base(messages) { }
         public BravuraTonalityException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
