f462b9437df5f75995cdc9ea09e24442a2ec5e83
Erik Muir <erikdmuir@gmail.com>
2019-12-20 22:08:14 -0600

    move IReadOnlyOrderedDictionary interface to separate file

--- Bravura.Tonality/Helpers/ReadOnlyOrderedDictionary.cs
+++ Bravura.Tonality/Helpers/ReadOnlyOrderedDictionary.cs
@@ -8,31 +8,7 @@ namespace Bravura.Tonality
 {
     /// <inheritdoc />
     /// <summary>
-    /// Represents a collection of key/value pairs that are accessible by the key or index.
-    /// </summary>
-    /// <typeparam name="TKey">The type of keys</typeparam>
-    /// <typeparam name="TValue">The type of values</typeparam>
-    public interface IReadOnlyOrderedDictionary<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>
-    {
-        /// <summary>
-        /// The value of the element at the given index.
-        /// </summary>
-        TValue this[int index] { get; }
-
-        /// <summary>
-        /// The value of the element with the given key.
-        /// </summary>
-        new TValue this[TKey key] { get; }
-
-        /// <summary>
-        /// Find the position of an element by key. Returns -1 if the dictionary does not contain an element with the given key.
-        /// </summary>
-        int IndexOf(TKey key);
-    }
-
-    /// <inheritdoc />
-    /// <summary>
-    /// Represents a collection of key/value pairs that are accessible by the key or index.
+    /// Represents an ordered collection of key/value pairs that are accessible by the key or index.
     /// </summary>
     /// <typeparam name="TKey">The type of keys. Cannot be <see cref="T:System.Int32" /></typeparam>
     /// <typeparam name="TValue">The type of values.</typeparam>
