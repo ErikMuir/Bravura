d952d0c85b216d8d6ea07214bb9b7af053dddac9
Erik Muir <erikdmuir@gmail.com>
2019-12-22 00:21:00 -0600

    move HashCode to Bravura.Common

--- Bravura.Common/HashCode.cs
+++ Bravura.Common/HashCode.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 
-namespace Bravura.Tonality
+namespace Bravura.Common
 {
     public struct HashCode
     {
--- Bravura.Tonality/Accidental.cs
+++ Bravura.Tonality/Accidental.cs
@@ -1,4 +1,5 @@
 using System;
+using Bravura.Common;
 
 namespace Bravura.Tonality
 {
