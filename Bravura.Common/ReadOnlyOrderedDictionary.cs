d68c8d0b0bbf37b1c0a1c9ac8281fe63d626470b
Erik Muir <erikdmuir@gmail.com>
2019-12-22 00:20:42 -0600

    move ReadOnlyOrderedDictionary to Bravura.Common

--- Bravura.Common/ReadOnlyOrderedDictionary.cs
+++ Bravura.Common/ReadOnlyOrderedDictionary.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
-namespace Bravura.Tonality
+namespace Bravura.Common
 {
     /// <inheritdoc />
     /// <summary>
